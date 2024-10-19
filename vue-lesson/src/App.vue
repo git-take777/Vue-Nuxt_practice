1c915c428240ad6491d800465611fe6828b852e1
git-take777 <git.coding.life@gmail.com>
2024-10-19 13:20:12 +0900

    refや関数の使い方、v-bind、イベント発火時の使い方

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,47 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const title = 'Vue.js Course'
+    const price1 = ref(9.99)
+    const count = ref(1)
+    const count2 = ref(2)
+    const message = ref('hello')
+    const baseURL = ref('https://vuejs.org')
+    const id = ref('vue-link')
+    const VueId = ref('vue-id')
+    function increment() {
+      price1.value = price1.value - 1
+    }
+
+    // ここで変数を返す
+    return {
+      title,
+      price1,
+      count,
+      count2,
+      message,
+      increment,
+      id,
+      baseURL,
+      VueId,
+    }
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <p>title: {{ title }}</p>
+    <p>price: ${{ price1 - 1 }}</p>
+    <div>{{ count + count2 }}</div>
+    <div v-html="message"></div>
+    <!-- v-bindは省略可能→属性値と設定値が同じ文字なら省略可能 -->
+    <a :id="id" v-bind:href="baseURL">Vue.js</a>
+    <a v-bind="{ id: VueId, href: baseURL }">Vue.js</a>
+    <!-- 参考サイト(v-onの種類)https://vuejs.org/api/built-in-directives.html#v-on -->
+    <button @click="count = $event.clientX">incrementBtn</button>
+    <button @click="increment">incrementBtn</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- vue-lesson/src/main.js
+++ vue-lesson/src/main.js
@@ -2,5 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import conditions from './components/conditions.vue'
 createApp(App).mount('#app')
